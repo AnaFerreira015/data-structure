445430a43bea0a95f71b7c84a3feac3dbe557a5a
anaferreira015 <dasilvaferreira.af@hotmail.com>
2019-05-28 22:20:59 -0300

    Fix indentation

--- High order functions/map.c
+++ High order functions/map.c
@@ -5,13 +5,14 @@ void printing_array(int array[], int size, int i);
 
 void reading_array(int array[], int size, int i);
 
-int square (int number);
+int square(int number);
 
 int factorial(int number);
 
-void map(int(*function)(int n), int array[], int size, int i);
+void map(int (*function)(int n), int array[], int size, int i);
 
-int main () {
+int main()
+{
     int size;
 
     printf("Size: \n");
@@ -22,42 +23,52 @@ int main () {
     reading_array(numbers, size, 0);
 
     printf("\nSquare: \n");
-    map(square, numbers, size, 0);
-    
+    map(factorial, numbers, size, 0);
+
     printing_array(numbers, size, 0);
     return 0;
 }
 
-void printing_array(int array[], int size, int i) {
-    if(i < size) {
+void printing_array(int array[], int size, int i)
+{
+    if (i < size)
+    {
         printf("[%d] = %d\n", i, array[i]);
         printing_array(array, size, ++i);
     }
 }
 
-void reading_array(int array[], int size, int i) {
-    if(i < size) {
+void reading_array(int array[], int size, int i)
+{
+    if (i < size)
+    {
         printf("[%d] = ", i);
         scanf("%d", &array[i]);
         reading_array(array, size, ++i);
     }
 }
 
-int square (int number) {
-    return number*number;
+int square(int number)
+{
+    return number * number;
 }
 
-int factorial(int number) {
-    if(number <= 1) {
+int factorial(int number)
+{
+    if (number <= 1)
+    {
         return 1;
     }
-    else {
-        return number*factorial(number-1);
+    else
+    {
+        return number * factorial(number - 1);
     }
 }
 
-void map(int(*function)(int n), int array[], int size, int i) {
-    if(i < size) {
+void map(int (*function)(int n), int array[], int size, int i)
+{
+    if (i < size)
+    {
         array[i] = (*function)(array[i]);
         map(function, array, size, ++i);
     }
