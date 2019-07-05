5b4598857fb6baec312e7a89080293081a5589c9
anaferreira015 <dasilvaferreira.af@hotmail.com>
2019-07-05 10:39:13 -0300

    Indent code

--- The Huxley/415 - Balanceamento de Parênteses.c
+++ The Huxley/415 - Balanceamento de Parênteses.c
@@ -4,41 +4,49 @@
 
 #define MAX_SIZE 256
 
-typedef struct NODE {
+typedef struct NODE
+{
     char character;
     struct NODE *next;
 } NODE;
 
-typedef struct STACK {
+typedef struct STACK
+{
     struct NODE *top;
 } STACK;
 
-STACK *creating_stack() {
-    STACK *new_stack = (STACK *) malloc(sizeof(STACK));
+STACK *creating_stack()
+{
+    STACK *new_stack = (STACK *)malloc(sizeof(STACK));
     new_stack->top = NULL;
 
     return new_stack;
 }
 
-void push(STACK *stack, char character){
-    NODE *new_top = (NODE*)malloc(sizeof(NODE));
+void push(STACK *stack, char character)
+{
+    NODE *new_top = (NODE *)malloc(sizeof(NODE));
 
     new_top->character = character;
     new_top->next = stack->top;
     stack->top = new_top;
 }
 
-int isEmpty(STACK *stack){
+int isEmpty(STACK *stack)
+{
     return (stack->top == NULL);
 }
 
-NODE *pop(STACK *stack) {
-    NODE *aux = (NODE*)malloc(sizeof(NODE));
+NODE *pop(STACK *stack)
+{
+    NODE *aux = (NODE *)malloc(sizeof(NODE));
 
-    if(isEmpty(stack)){
+    if (isEmpty(stack))
+    {
         printf("Stack underflow\n");
     }
-    else  {
+    else
+    {
         aux = stack->top;
         stack->top = stack->top->next;
         aux->next = NULL;
@@ -47,23 +55,29 @@ NODE *pop(STACK *stack) {
     }
 }
 
-NODE *peek(STACK *stack){
-    if(isEmpty(stack)){
+NODE *peek(STACK *stack)
+{
+    if (isEmpty(stack))
+    {
         printf("Stack underflow\n");
     }
-    else {
+    else
+    {
         return stack->top;
     }
 }
 
-void printing(NODE *node){
-    if(node != NULL){
+void printing(NODE *node)
+{
+    if (node != NULL)
+    {
         printf("%c\n", node->character);
         return printing(node->next);
     }
 }
 
-int main () {
+int main()
+{
     int qtd_strings, i, size_string, j;
 
     char sequency[MAX_SIZE];
@@ -72,59 +86,34 @@ int main () {
 
     STACK *top = creating_stack();
 
-    // for(j = 0; j < qtd_strings; j++){         
-        scanf("%[^\n]s", sequency);
-        size_string = strlen(sequency);
-        if(sequency[0] == '(' || sequency[0] == '['){
-            push(top, sequency[0]);
-            for(i = 1; i < size_string; i++) {
-                if(sequency[i] == '(' || sequency[i] == '[') {
-                    push(top, sequency[i]);
-                }
-                else if(sequency[i] == ')' || sequency[i] == ']')
-                {
-                    pop(top);
-                }
-
+    scanf("%[^\n]s", sequency);
+    size_string = strlen(sequency);
+    if (sequency[0] == '(' || sequency[0] == '[')
+    {
+        push(top, sequency[0]);
+        for (i = 1; i < size_string; i++)
+        {
+            if (sequency[i] == '(' || sequency[i] == '[')
+            {
+                push(top, sequency[i]);
             }
-            if(isEmpty(top)){
-                printf("Yes\n");
-            }
-            else {
-                printf("No\n");
+            else if (sequency[i] == ')' || sequency[i] == ']')
+            {
+                pop(top);
             }
         }
-        else {
+        if (isEmpty(top))
+        {
+            printf("Yes\n");
+        }
+        else
+        {
             printf("No\n");
         }
-    // }
-
-    // for (i = 0; i < qtd_strings; i++){
-    //     scanf("%[^\n]s", sequency);
-    //     size_string = strlen(sequency);
-
-    //     // STACK *top = creating_stack();
-
-    //     if(sequency[0] == '(' || sequency[0] == '['){
-    //         push(top, sequency[0]);
-    //         for(j = 1; j < size_string; j++){
-    //             if(sequency[j] == '(' || sequency[j] == '[') {
-    //                 push(top, sequency[j]);
-    //             }
-    //             else {
-    //                 pop(top);
-    //             }
-    //         }
-    //         if(isEmpty(top)){
-    //             printf("Yes\n");
-    //         }
-    //         else {
-    //             printf("No\n");
-    //         }
-    //     }
-    //     else {
-    //         printf("No\n");
-    //     }
-    // }
+    }
+    else
+    {
+        printf("No\n");
+    }
     return 0;
 }
